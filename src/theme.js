f8fdc925c6ce3606f37391a2dc15efa648575577
Josh <jbatley12@gmail.com>
2025-02-13 15:00:02 +0000

    script fix

---
+++ src/theme.js
@@ -0,0 +1,24 @@
+const preferDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+document.addEventListener('DOMContentLoaded', function () {
+  var storedState = localStorage.getItem('joshbatley.theme.changed');
+  var isChecked = storedState === null ? false : storedState === 'true';
+  document.getElementById('theme-selector').checked = isChecked;
+  updateCodeHightlighting(isChecked ? !preferDark : preferDark);
+
+});
+
+function updateLocalStorage(isChecked) {
+  localStorage.setItem('joshbatley.theme.changed', isChecked.toString());
+  updateCodeHightlighting(isChecked ? !preferDark : preferDark);
+}
+
+function updateCodeHightlighting(preferDark) {
+  const themeLink = document.getElementById('codeHighlighting');
+  if (preferDark) {
+    themeLink.href = "https://unpkg.com/@highlightjs/cdn-assets@11.9.0/styles/github-dark.min.css"
+  } else {
+    themeLink.href = "https://unpkg.com/@highlightjs/cdn-assets@11.9.0/styles/github.min.css"
+  }
+}
+hljs.highlightAll();
